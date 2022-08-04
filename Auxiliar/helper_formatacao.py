2f7e42a0d0eede525fa82b241d45f7cf3a924dde
Pedro Favoreto Gaya <pedrogayaf@gmail.com>
2022-08-04 09:43:04 -0300

    Corrige _print_chave_valor()

--- Auxiliar/helper_formatacao.py
+++ Auxiliar/helper_formatacao.py
@@ -18,9 +18,9 @@ def print_header(titulo, divisor="=", quantidade=30):
 
 
 def _print_chave_valor(chave, valor, recuo, incremento, char_recuo):
-    """Função auxiliar para print_json()
+    """Função auxiliar para print_json().
     
-    Apresenta um par de chave, valor de um JSON com formatação.
+    Apresenta, com formatação, um par de chave-valor de um objeto JSON.
 
     Args:
         chave (str): O nome da chave.
@@ -32,17 +32,26 @@ def _print_chave_valor(chave, valor, recuo, incremento, char_recuo):
     # Recuar linha pelo número de caracteres de recuo
     print(char_recuo*recuo, end="")
     
-    # Apresentar chave
-    print('"' + chave + '":  ', end="")
+    # Apresentar chave, se houver
+    if chave:
+        print('"' + chave + '":  ', end="")
     
-    # Se o valor for um dicionário ou lista
+    # Se o valor for um dicionário
     if type(valor) is dict:
 
         # Imprime chave de abertura
-        print("{")
+        print("{", end="")
         
-        # Recursão para os items do valor no próximo nível de recuo
+        # Se for um dicionário vazio
+        if len(valor) == 0:
+            # Fecha a chave e encerra este item
+            print("}")
+            return
+        else:
+            # Quebra de linha
+            print()
         
+        # Recursão para os items do dicionário, no próximo nível de recuo
         for c, v in valor.items():
             _print_chave_valor(c, v, recuo+incremento, incremento, char_recuo)
         
@@ -59,34 +68,16 @@ def _print_chave_valor(chave, valor, recuo, incremento, char_recuo):
         
         # Se for uma lista vazia
         if len(valor) == 0:
-            # Fecha colchete e encerra este item
+            # Fecha o colchete e encerra este item
             print("]")
             return
         else:
             # Quebra de linha
             print()
         
-        # Para cada item do valor, apresentar como um dicionário cada item
+        # Recursão para os itens da lista, no próximo nível de recuo, sem chave
         for item in valor:
-            # Aumenta o nível de recuo
-            recuo += incremento
-            
-            # Recuar linha pelo número de caracteres de recuo
-            print(char_recuo*recuo, end="")
-            # Imprime chave de abertura
-            print("{") 
-            
-            # Recursão para os items do valor no próximo nível de recuo
-            for c, v in item.items():
-                _print_chave_valor(c, v, recuo+incremento, incremento, char_recuo)
-            
-            # Recuar linha pelo número de caracteres de recuo
-            print(char_recuo*recuo, end="")
-            # Imprime chave de fechamento
-            print("}")
-       
-        # Retorna ao nível de recuo anterior
-        recuo -= incremento
+            _print_chave_valor(None, item, recuo+incremento, incremento, char_recuo)
         
         # Recuar linha pelo número de caracteres de recuo
         print(char_recuo*recuo, end="")
