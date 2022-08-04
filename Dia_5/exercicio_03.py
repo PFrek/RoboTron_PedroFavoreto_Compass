c44cf12e35487507c7b3133ec33793117423dfa9
Pedro Favoreto Gaya <pedrogayaf@gmail.com>
2022-08-04 10:35:56 -0300

    Adiciona funcionalidade para entradas decimais

--- Dia_5/exercicio_03.py
+++ Dia_5/exercicio_03.py
@@ -6,6 +6,10 @@ Enunciado:
 
 Autor:
     Pedro Favoreto Gaya - 27/07/2022
+    
+Referências:
+    Verificar se um float contém um número inteiro:
+    https://note.nkmk.me/en/python-check-int-float/
 """
 
 ###
@@ -20,25 +24,30 @@ from Auxiliar.helper_formatacao import print_header
 
 # Função main
 def main():
-    
 
     # Entrada dos valores
     print_header("ENTRADA")
 
     try:
-        valor_1, valor_2 = map(int, input("Digite os dois valores inteiros: ").split())
+        # Recebe e tenta converter as entradas para float
+        valor_1, valor_2 = map(float, input("Digite os dois valores: ").split())
     except ValueError as err:
+        # Saída de erro
         print("Erro: Entrada inválida. -- ", err)
         return
     
     # Soma dos valores
     soma = valor_1 + valor_2
     
-    # String que indicará se a soma é par ou ímpar
+    # String que indicará se a soma é par, ímpar ou indefinido
     par_ou_impar = ""
     
+    # Se a soma não resultar em um número inteiro
+    if not soma.is_integer():
+        par_ou_impar = "INDEFINIDO"
+    
     # Se a soma dos valores for par
-    if soma % 2 == 0:
+    elif soma % 2 == 0:
         par_ou_impar = "PAR"
        
     # Se não, será ímpar
@@ -60,27 +69,57 @@ if __name__ == '__main__':
 TEST CASES
 ========================================================================
 
-TC-01:
+TC-01: (Entrada inteiros, resultado par)
 ------------------------------------------------------------------------
 [Entrada]
 5 3
 ------------------------------------------------------------------------
 [Saída esperada]
-5 + 3 = 8 --> PAR
+5.0 + 3.0 = 8.0 --> PAR
 
 ========================================================================
 
-TC-02:
+TC-02: (Entrada inteiros, resultado ímpar)
 ------------------------------------------------------------------------
 [Entrada]
 2 5
 ------------------------------------------------------------------------
 [Saída esperada]
-2 + 5 = 7 --> ÍMPAR
+2.0 + 5.0 = 7.0 --> ÍMPAR
 
 ========================================================================
 
-TC-03:
+TC-03: (Entrada decimal, resultado par)
+------------------------------------------------------------------------
+[Entrada]
+1.2 2.8
+------------------------------------------------------------------------
+[Saída esperada]
+1.2 + 2.8 = 4.0 --> PAR
+
+========================================================================
+
+TC-04: (Entrada decimal, resultado ímpar)
+------------------------------------------------------------------------
+[Entrada]
+1.2 1.8
+------------------------------------------------------------------------
+[Saída esperada]
+1.2 + 1.8 = 3.0 --> ÍMPAR
+
+========================================================================
+
+TC-05: (Entrada decimal, resultado decimal)
+------------------------------------------------------------------------
+[Entrada]
+1.5 1.7
+------------------------------------------------------------------------
+[Saída esperada]
+1.5 + 1.7 = 3.2 --> INDEFINIDO
+
+========================================================================
+
+TC-06: (Entrada inválida)
 ------------------------------------------------------------------------
 [Entrada]
 1 cinco
